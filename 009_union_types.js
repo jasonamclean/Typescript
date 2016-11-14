d8920e8dab34f17d562b2d0a4d0318a294e6a555
Jason McLean <jasonamclean@hotmail.com>
2016-11-14 12:44:07 -0700

    Typescript union types

---
+++ 009_union_types.js
@@ -0,0 +1,10 @@
+var players = ["Altuve", "Corerra", "Bregman"];
+var player_numbers = [25, 3, 2];
+console.log(players);
+console.log(player_numbers);
+var names;
+names = ["Jason McLean", "Natashia McLean"];
+console.log(names);
+names = "Nora McLean";
+console.log(names);
+//# sourceMappingURL=009_union_types.js.map
---
+++ 009_union_types.ts
@@ -0,0 +1,11 @@
+type PlayerArray = Array<string|number>;
+let players : PlayerArray = ["Altuve", "Corerra", "Bregman"];
+let player_numbers : PlayerArray = [25, 3, 2];
+console.log(players);
+console.log(player_numbers);
+
+var names : string[]|string;
+names = ["Jason McLean", "Natashia McLean"];
+console.log(names);
+names = "Nora McLean";
+console.log(names);
