9b6f78da59a18d0be67a4864f8bd92e6360704e0
Jason McLean <jasonamclean@hotmail.com>
2016-11-26 15:24:18 -0800

    Typescript objects

---
+++ 022_objects.ts
@@ -0,0 +1,13 @@
+var realUser = {
+	email: 'test@test.com',
+	firstName: 'Henry',
+	lastName: 'Jumpmaster',
+	sayHi() {
+		return "Hey there!";
+	}
+};
+
+console.log(realUser.email);
+console.log(realUser.firstName);
+console.log(realUser.lastName);
+console.log(realUser.sayHi());
