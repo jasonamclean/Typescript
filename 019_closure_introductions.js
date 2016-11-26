c4a5b25458abd0eb89037cd8119fc357259f8ce0
Jason McLean <jasonamclean@hotmail.com>
2016-11-26 15:21:34 -0800

    Typescript closure introductions

---
+++ 019_closure_introductions.js
@@ -0,0 +1,37 @@
+// functions have access to any public variables in the outer scope
+// function nameFunction(name: string) : void {
+// 	var n : string = name;
+// 	function printName() {
+// 		console.log(n);
+// 	}
+// 	printName();
+// }
+// nameFunction('Henry');
+// * * *
+// The inner function maintains access to the outer scope even AFTER the values are returned!
+// function nameFunction(name: string) {
+// 	var n : string = name;
+// 	return function() {
+// 		console.log(n);
+// 	}
+// }
+// var nameAgain = nameFunction('Marty');
+// nameAgain()
+// * * *
+function lineup() {
+    var nowBatting = 1;
+    return {
+        nextBatter: function () { nowBatting++; },
+        currentBatter: function () { return nowBatting; }
+    };
+}
+var batters = lineup();
+console.log(batters.currentBatter());
+batters.nextBatter();
+console.log(batters.currentBatter());
+batters.nextBatter();
+console.log(batters.currentBatter());
+batters.nextBatter();
+var pitchers = lineup();
+console.log(pitchers.currentBatter());
+//# sourceMappingURL=019_closure_introductions.js.map
